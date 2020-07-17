1eed15cf64bd135ce0a7d5917851ac813bca7950
Conan <conan.chang@pentium.network>
2020-07-17 16:51:22 +0800

    fix: increase max token size

--- executor/logging.go
+++ executor/logging.go
@@ -11,6 +11,9 @@ func bindLoggingPipe(name string, pipe io.Reader, output io.Writer) {
 	log.Printf("Started logging %s from function.", name)
 
 	scanner := bufio.NewScanner(pipe)
+	buf := make([]byte, 2048)
+	scanner.Buffer(buf, 128*1024*1024)
+
 	logger := log.New(output, log.Prefix(), log.Flags())
 
 	go func() {
